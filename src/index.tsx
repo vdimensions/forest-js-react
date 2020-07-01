ec1352ffce89e5d68d06c9529d60a856ec3f47db
ivaylo5ev <ivaylo5ev@gmail.com>
2020-07-02 00:15:44 +0300

    - fixed exports

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,4 @@
 export { ForestApp }  from './forest-app';
+export { useNavigate, useCommand} from './hooks';
+export { RegisterComponent } from './register-component';
+export { Region } from './region';
