c4b07fb828a3c85bea8248fada59b8fbb602f91d
ivaylo5ev <ivaylo5ev@gmail.com>
2021-12-31 21:02:51 +0200

    - exposed more types to enable overriding the forest hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 export { ForestApp, LocationNavigator }  from './forest-app';
 export type { TForestReactCommand as ForestReactCommand } from './hooks';
-export { useNavigate, useCommand } from './hooks';
+export { ForestHooksContext, DefaultForestHooks, useNavigate, useCommand } from './hooks';
+export { ForestStoreContext } from './store';
 export { Region } from './region';
 export { ForestMappingView, ForestView } from './forest-view';
